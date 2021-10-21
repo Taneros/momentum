ccd5277e177676e926b7c03306e8951d747aaf5c
Taneros <fricentaurus@GMAIL.COM>
2021-10-21 16:59:31 +0300

    feat: add greeting

--- momentum/js/clock.js
+++ momentum/js/clock.js
@@ -1,14 +1,20 @@
 console.log(`clock.js loaded`)
 
+let log = console.log
+
 export function clock() {
   const TIME = document.querySelector('.time')
   const DATE = document.querySelector('.date')
+  const GREETING = document.querySelector('.greeting')
+  const NAME = document.querySelector('.name')
+
   // display time
   function showTime() {
     const DATE_OBJ = new Date()
     const CURRENTTIME = DATE_OBJ.toLocaleTimeString()
     TIME.textContent = `${CURRENTTIME}`
     showDate()
+    showGreeting()
     setTimeout(showTime, 1000)
   }
   showTime()
@@ -23,4 +29,30 @@ export function clock() {
     const WEEK = CURRENTDATE[0].trim()
     DATE.textContent = `${WEEK}, ${MONTH}`
   }
+
+  // Greeting
+  function getTimeOfDay() {
+    const DATE_OBJ = new Date()
+    const TIME_NOW = DATE_OBJ.getHours()
+    if (TIME_NOW > 12) return 'Good afternoon'
+    if (TIME_NOW > 12 && TIME_NOW < 0) return 'Good evening'
+    else return 'Good morning'
+    log(TIME_NOW)
+  }
+
+  function showGreeting() {
+    GREETING.textContent = `${getTimeOfDay()}`
+  }
+
+  // Name input
+  function setLocalStorage() {
+    localStorage.setItem('name', NAME.value)
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  function getLocalStorage() {
+    const LOCAL_NAME = localStorage.getItem('name')
+    if (LOCAL_NAME) NAME.value = LOCAL_NAME
+  }
+  window.addEventListener('load', getLocalStorage)
 }
